aa67b84dd8be68f7a361da352d4bd4abe69efca8
litlighilit <litlighilit@foxmail.com>
2024-08-23 11:25:03 +0800

    impr: fixup by wrap as a function `getLastChangeMsg`

--- step/get_msg_of_changed_line.sh
+++ step/get_msg_of_changed_line.sh
@@ -5,9 +5,14 @@ file="$dir/$basefile"
 n=`grep -n '^collections:$' "$file" | cut -d : -f 1`
 echo $n
 
-pushd "$dir"
-commithash=`git blame -L $n "$basefile" | cut -d ' ' -f 1`
-# 如果想查看整个commit
-# git show "$commithash"
-git log -1 --format='%s'  # 只输出 commit message
-popd
+function getLastChangeMsg(){
+    set -e
+    loca dir="$1" basefile="$2" n="$3" commithash
+    pushd "$dir"
+    commithash=`git blame -L $n "$basefile" | cut -d ' ' -f 1`
+    # 如果想查看整个commit
+    # git show "$commithash"
+    git log -1 --format='%s'  # 只输出 commit message
+    popd
+}
+getLastChangeMsg "$dir" "$basefile" "$n"
